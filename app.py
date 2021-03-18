8e2ea7cc27490ef32d3009da154a2acf87f8ebcd
Ortlinde <31532710+Ortlinde@users.noreply.github.com>
2021-03-18 21:59:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def callback():
 
 
 # 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message = TextMessage)
 def handle_message(event):
     msg = event.message.text
     if '最新合作廠商' in msg:
