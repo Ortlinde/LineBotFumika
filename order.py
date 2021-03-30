55974eaf1436ce50cdce324f5d9dcd32f13893a2
Ortlinde <31532710+Ortlinde@users.noreply.github.com>
2021-03-30 14:58:02 +0800

    Create order.py

---
+++ order.py
@@ -0,0 +1,104 @@
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+
+# 點餐
+def order():
+    message = TemplateSendMessage(
+        alt_text='好消息來囉～',
+        template=ButtonsTemplate(
+            thumbnail_image_url="https://pic2.zhimg.com/v2-de4b8114e8408d5265503c8b41f59f85_b.jpg",
+            title="是否要進行抽獎活動？",
+            text="輸入生日後即獲得抽獎機會",
+            actions=[
+                DatetimePickerTemplateAction(
+                    label="請選擇生日",
+                    data="input_birthday",
+                    mode='date',
+                    initial='1990-01-01',
+                    max='2019-03-10',
+                    min='1930-01-01'
+                ),
+                MessageTemplateAction(
+                    label="看抽獎品項",
+                    text="有哪些抽獎品項呢？"
+                ),
+                URITemplateAction(
+                    label="免費註冊享回饋",
+                    uri="https://tw.shop.com/nbts/create-myaccount.xhtml?returnurl=https%3A%2F%2Ftw.shop.com%2F"
+                ),
+                PostbackTemplateAction(
+                    label='下一頁',
+                    data='next_page_1'
+                )
+            ]
+        )
+    )
+    return message
+
+def largeOrderPanel():
+    message = TemplateSendMessage(
+        alt_text='一則旋轉木馬按鈕訊息',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Number_1_in_green_rounded_square.svg/200px-Number_1_in_green_rounded_square.svg.png',
+                    title='這是第一塊模板',
+                    text='一個模板可以有三個按鈕',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='回傳一個訊息',
+                            data='將這個訊息偷偷回傳給機器人'
+                        ),
+                        MessageTemplateAction(
+                            label='用戶發送訊息',
+                            text='我知道這是1'
+                        ),
+                        URITemplateAction(
+                            label='進入1的網頁',
+                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Number_1_in_green_rounded_square.svg/200px-Number_1_in_green_rounded_square.svg.png'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuo7n2_HNSFuT3T7Z9PUZmn1SDM6G6-iXfRC3FxdGTj7X1Wr0RzA',
+                    title='這是第二塊模板',
+                    text='副標題可以自己改',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='回傳一個訊息',
+                            data='這是ID=2'
+                        ),
+                        MessageTemplateAction(
+                            label='用戶發送訊息',
+                            text='我知道這是2'
+                        ),
+                        URITemplateAction(
+                            label='進入2的網頁',
+                            uri='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Number_2_in_light_blue_rounded_square.svg/200px-Number_2_in_light_blue_rounded_square.svg.png'
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Number_3_in_yellow_rounded_square.svg/200px-Number_3_in_yellow_rounded_square.svg.png',
+                    title='這是第三個模塊',
+                    text='最多可以放十個',
+                    actions=[
+                        PostbackTemplateAction(
+                            label='上一頁',
+                            data='next_page_0'
+                        ),
+                        PostbackTemplateAction(
+                            label='下一頁',
+                            data='next_page_1'
+                        ),
+                        PostbackTemplateAction(
+                            label='結束',
+                            data='end'
+                        )
+                    ]
+                )
+            ]
+        )
+    )
+    return message
