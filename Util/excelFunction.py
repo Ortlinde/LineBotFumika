b26d8f7a50f1fcd9e4a3f8434bb4d52a29ba92af
Ortlinde <31532710+Ortlinde@users.noreply.github.com>
2021-04-30 11:14:47 +0800

    Update excelFunction.py

--- Util/excelFunction.py
+++ Util/excelFunction.py
@@ -1,17 +1,17 @@
 import requests
 import json
+import time
 
 with open('./JSON/config.json', mode='r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
-def SendDataToGoogleSheet():
+def sendDataToGoogleSheet(name, value):
     url = jdata['Gas']['Get'][0]['baseExcel']
-    payload = {'ID': 2,
-               'DATE': 20210429,
-               'NAME': 'A',
-               'VALUE': 'B'
-              }
-    resp = requests.get(url, params=payload)
+    payload = {
+        'DATE': time.strftime("%Y-%m-%d", time.localtime()),
+        'NAME': name,
+        'VALUE': value }
+    resp = requests.post(url, params=payload)
     print(resp.text)
 
 # return a dict of sheet
@@ -20,21 +20,3 @@ def getDataFromGoogleSheet():
     resp = requests.get(url, 0)
 
     return json.loads(resp.text)
-
-# keyList = []
-# valueList = []
-# reactDict = {}
-
-# message = ''
-# kvData = getDataFromGoogleSheet()
-# for outer in kvData:
-#     for inner in kvData.get(outer):
-#         if inner == 'NAME':
-#             keyList.append(kvData.get(outer).get(inner))
-#         if inner == 'VALUE':
-#             valueList.append(kvData.get(outer).get(inner))
-    
-# for i in range(len(keyList)):
-#     reactDict[keyList[i]] = valueList[i]
-
-# print(reactDict.get('A'))
