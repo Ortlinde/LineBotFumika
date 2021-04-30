8982c1429a7812fc2256f5079c1bc85d7c932d1a
Ortlinde <31532710+Ortlinde@users.noreply.github.com>
2021-05-01 05:56:24 +0800

    Update openJS.py

--- Util/openJS.py
+++ Util/openJS.py
@@ -1,9 +1,5 @@
 # open javaScript file
 def js_from_file(file_name):
-    """
-    讀取js檔案
-    :return:
-    """
     with open(file_name, 'r', encoding='UTF-8') as file:
         result = file.read()
 
