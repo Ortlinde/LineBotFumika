b1bebdfe63529441547f561245c09563404b9cff
Ortlinde <31532710+Ortlinde@users.noreply.github.com>
2021-05-06 19:01:14 +0800

    Update excelFunction.py

--- Util/excelFunction.py
+++ Util/excelFunction.py
@@ -1,22 +1,12 @@
 import requests
-import json
 import time
 
-with open('./JSON/config.json', mode='r', encoding='utf8') as jfile:
-    jdata = json.load(jfile)
-
 # send a column of data to google sheet
-def sendDataToGoogleSheet(name, value):
-    url = jdata['Gas']['Get'][0]['baseExcel']
-    payload = {
-        'DATE': time.strftime("%Y-%m-%d", time.localtime()),
-        'NAME': name,
-        'VALUE': value }
+def sendDataToGoogleSheet(url, payload):
     resp = requests.post(url, params=payload)
 
 # return a dict of sheet
 def getDataFromGoogleSheet():
-    url = jdata['Gas']['Get'][0]['baseExcel']
     resp = requests.get(url, 0)
 
     return json.loads(resp.text)
