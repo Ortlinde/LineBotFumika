e5a0c9d60f450409f52e293c2ca92b8e2b14a218
Ortlinde <31532710+Ortlinde@users.noreply.github.com>
2021-05-06 23:50:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def handle_message(event):
             ordering = False
         elif msg in restaurantName :
             r = Restaurant(msg)
-            getOrder(r)
+            getOrder(line_bot_api,r)
             #line_bot_api.reply_message(event.reply_token, TextSendMessage(text= r.toString()))
             return
 
