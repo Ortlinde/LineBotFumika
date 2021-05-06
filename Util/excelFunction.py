aa7489cf0b4ecc78ebf47ec065f8850fd6ff8c20
Ortlinde <31532710+Ortlinde@users.noreply.github.com>
2021-05-06 19:08:25 +0800

    Update excelFunction.py

--- Util/excelFunction.py
+++ Util/excelFunction.py
@@ -6,7 +6,7 @@ def sendDataToGoogleSheet(url, payload):
     resp = requests.post(url, params=payload)
 
 # return a dict of sheet
-def getDataFromGoogleSheet():
+def getDataFromGoogleSheet(url):
     resp = requests.get(url, 0)
 
     return json.loads(resp.text)
