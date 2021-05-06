b99b959b07292323b2438857ac87bd3ad0c83873
Ortlinde <31532710+Ortlinde@users.noreply.github.com>
2021-05-06 21:43:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def callback():
 # handle message
 @handler.add(MessageEvent, message = TextMessage)
 def handle_message(event):
-    global ordering, orderCalled
+    global message, ordering, orderCalled
     msg = event.message.text 
     
     if ordering == True:
