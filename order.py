ddcfe8037985136f7127ff71242ffb8de1fb4b85
Ortlinde <31532710+Ortlinde@users.noreply.github.com>
2021-05-07 08:04:05 +0800

    Update order.py

--- order.py
+++ order.py
@@ -11,7 +11,7 @@ def order():
         template=ButtonsTemplate(
             thumbnail_image_url="https://www.kamigo.tw/assets/kamigo-c3b10dff4cdb60fa447496b22edad6c32fffde96de20262efba690892e4461e8.png",
             title="今天想吃什麼呢？",
-            text="10分鐘內未點單請重新呼叫",
+            text="10分鐘內未點單請重新呼叫,點單時請按照\"品項;單價;數量\"的順序輸入,如要結帳請輸入\"c\"",
             actions=[
                 MessageTemplateAction(
                     label="今天想吃古今中外",
@@ -49,4 +49,4 @@ def getOrder(api, token, msg):
             preview_image_url='https://example.com/preview.jpg'
             )
         api.reply_message(token, message)
-    api.reply_message(token, TextSendMessage(text='請按照\"品項;單價;數量\"的順序輸入,如要結帳請輸入\"c\"'))
+    #api.reply_message(token, TextSendMessage(text='點單時請按照\"品項;單價;數量\"的順序輸入,如要結帳請輸入\"c\"'))
