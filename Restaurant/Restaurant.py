1a8c2ed649ce134a8545f6e7ccbea1dd5b8f9512
Ortlinde <31532710+Ortlinde@users.noreply.github.com>
2021-05-12 10:09:28 +0800

    Create Restaurant.py

---
+++ Restaurant/Restaurant.py
@@ -0,0 +1,16 @@
+class Restaurant:
+    def __init__(self,name,userId):
+        self.name = name
+        self.userId = userId
+        
+    def getName(self):
+        return self.name
+
+class EightWay(Restaurant):
+    pass
+
+class YiTing(Restaurant):
+    pass
+
+class GuJing(Restaurant):
+    pass
